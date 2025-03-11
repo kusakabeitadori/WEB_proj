b8e801606c409e4d8b08236e846059be0478500a
Norbert <wadjanorbert@gmail.com>
2025-03-11 20:23:41 +0000

    juste un petit test de retour

---
+++ Devoir/TPpers/openTP/exemple.php
@@ -0,0 +1,18 @@
+!<!DOCTYPE html>
+<html>
+<head>
+  <title>Mon projet avec Tailwind CSS</title>
+  <link href="./style.css" rel="stylesheet">
+</head>
+<body>
+  <h1 class="text-3xl font-bold underline">
+    Bonjour le monde !
+  </h1>
+  <p class="text-gray-700">
+    Ceci est un exemple de texte stylisé avec Tailwind CSS.
+  </p>
+  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+    Cliquez ici
+  </button>
+</body>
+</html>
